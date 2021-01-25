90ee5a04bb301928ef1767f120b47ffc02baee58
Nuno Fonseca <nuno_rgf@hotmail.com>
2021-01-25 15:30:38 +0000

    Adicionado Autenticação

---
+++ SitePessoal/Data/SitePessoalDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SitePessoal.Data
+{
+    public class SitePessoalDbContext : IdentityDbContext
+    {
+        public SitePessoalDbContext(DbContextOptions<SitePessoalDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
