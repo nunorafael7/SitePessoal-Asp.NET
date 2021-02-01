5f72a755825683286fc6a3b397e71705bcbc079e
Nuno Fonseca <nuno_rgf@hotmail.com>
2021-02-01 16:20:25 +0000

    alterações

---
+++ SitePessoal/Controllers/Escolas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SitePessoal.Controllers
+{
+    public class Escolas
+    {
+        public int EscolaID { get; set; }
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public string Nota { get; set; }
+    }
+}
